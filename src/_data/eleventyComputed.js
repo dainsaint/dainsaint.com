1fbd174ba04ec00608eb557fe75d05461dd89a9d
Dain Saint <dain@cipherprime.com>
2024-05-16 14:51:23 -0400

    favicons

--- src/_data/eleventyComputed.js
+++ src/_data/eleventyComputed.js
@@ -13,6 +13,8 @@ module.exports = {
   },
 
   design: {
+    brandColor: () => brandingColor,
+
     paginationColor: (data) => {
       if (data.pagination) return data.pagination.items[0].data.color;
       else return brandingColor;
